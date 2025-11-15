5af3e45f6a0ea4c50cfd1238bb0374419683eb05
Carlos Borges <carloos-boorges@hotmail.com>
2025-11-15 18:49:49 -0300

    refactor: atualizar mappers para compatibilidade com novos DTOs e relacionamentos

--- src/main/java/Sistema_Salao_de_Beleza/mapper/ClienteMapper.java
+++ src/main/java/Sistema_Salao_de_Beleza/mapper/ClienteMapper.java
@@ -1,6 +1,9 @@
 package Sistema_Salao_de_Beleza.mapper;
 import Sistema_Salao_de_Beleza.dto.ClienteDTO;
 import Sistema_Salao_de_Beleza.entity.ClienteModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
     ClienteModel toModel(ClienteDTO clienteDTO);
--- src/main/java/Sistema_Salao_de_Beleza/mapper/ProfissionalMapper.java
+++ src/main/java/Sistema_Salao_de_Beleza/mapper/ProfissionalMapper.java
@@ -1,7 +1,9 @@
 package Sistema_Salao_de_Beleza.mapper;
 import Sistema_Salao_de_Beleza.dto.ProfissionalDTO;
 import Sistema_Salao_de_Beleza.entity.ProfissionalModel;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ProfissionalMapper {
 
     ProfissionalModel toModel (ProfissionalDTO profissionalDTO);
